52f78db70adeb6a8b73d2d39f55f160b1cc77558
McKenna Oakes <dmckenna@ad.unc.edu>
2021-08-30 17:38:33 -0400

    progress exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -4,6 +4,6 @@ __author__= "730323932"
 
 name: str = input("What is your name? ")
 
-print( (name)+ " is a girlboss")
-print(("You're doing great ")+ (name))
-print(("You go ")+ (name) + " you can do it!")
+print( (name) , " is a girlboss!")
+print(("You're doing great ") , (name), "!")
+print(("You go ") , (name) , " you can do it!")
