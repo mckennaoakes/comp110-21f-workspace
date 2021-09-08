d2d24d10a56fbc7cad9a398f348e02a36cdd30d6
McKenna Oakes <dmckenna@ad.unc.edu>
2021-09-07 23:59:42 -0400

    progress exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -2,8 +2,8 @@
 
 __author__ = 730323932
 
-letter= input ("What letter do you want to search for?:")
-word= input("Enter a word:")
+letter = input ("What letter do you want to search for?:")
+word = input ("Enter a word:")
 
 i = 0
 a = (len(word))
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -3,15 +3,17 @@
 __author__ = 730323932
 
 
-beat = input( "Make a beat! Type a phrase that will be repeated." )
-num= int(input("How many times do you want this repeated?"))
-i= 1
-s= ""
+beat = input("Make a beat! Type a phrase that will be repeated.")
+num = int(input("How many times do you want this repeated?"))
+i = 1
+s = ""
+
+if num <= 0:
+    print("No beat...")
 
 while i <= num:
-    if i <= 0:
-        print( "No beat...")
-    else:
+    if i > 0:
         s = s + " " + beat
     i = i + 1
+
 print(s)
